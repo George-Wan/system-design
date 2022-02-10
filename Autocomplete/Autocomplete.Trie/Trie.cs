c916ef459d7c2118c031934a8cdeacb1e0a67996
George-Wan <georgewan1998@gmail.com>
2022-02-10 15:30:17 -0600

    Add Trie

---
+++ Autocomplete/Autocomplete.Trie/Trie.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Autocomplete.Trie
+{
+    public class Trie : ITire
+    {
+        private TrieNode _root;
+
+        public Trie()
+        {
+            _root = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            if (String.IsNullOrEmpty(word))
+            {
+                return;
+            }
+
+            TrieNode node = _root;
+            node.Pass++;
+
+            foreach (var w in word)
+            {
+                var path = w - 'a';
+                if (node.Children[path] == null)
+                {
+                    node.Children[path] = new TrieNode();
+                }
+
+                node = node.Children[path];
+                node.Pass++;
+            }
+
+            node.End++;
+        }
+
+
+        /// <summary>
+        /// Search a word in Trie and return the number of times of insertion
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns>the number of times of word insertion</returns>
+        public int Search(string word)
+        {
+            if (String.IsNullOrEmpty(word))
+            {
+                return 0;
+            }
+
+            TrieNode node = _root;
+
+            foreach (var w in word)
+            {
+                var path = w - 'a';
+                if (node.Children[path] == null)
+                {
+                    return 0;
+                }
+                node = node.Children[path];
+            }
+
+            return node.End;
+        }
+
+        /// <summary>
+        /// Get the number of prefix of inserted words
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns>the number of prefix of inserted words</returns>
+        public int GetPrefixNumber(string prefix)
+        {
+            if (String.IsNullOrEmpty(prefix))
+            {
+                return 0;
+            }
+
+            TrieNode node = _root;
+
+            foreach (var p in prefix)
+            {
+                var path = p - 'a';
+                if (node.Children[path] == null)
+                {
+                    return 0;
+                }
+
+                node = node.Children[path];
+            }
+
+            return node.Pass;
+        }
+
+        public void Delete(string word)
+        {
+            if (String.IsNullOrEmpty(word))
+            {
+                return;
+            }
+
+            if (Search(word) == 0)
+            {
+                return;
+            }
+
+            TrieNode node = _root;
+            node.Pass--;
+
+            foreach (var w in word)
+            {
+                var path = w - 'a';
+                if (--node.Children[path].Pass == 0)
+                {
+                    node.Children[path] = null;
+                    return;
+                }
+
+                node = node.Children[path];
+            }
+
+            node.End--;
+        }
+    }
+}
---
+++ Autocomplete/Autocomplete.Trie/TrieNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autocomplete.Trie
+{
+    class TrieNode
+    {
+        // The number of times a lower case letter passes a node of prefix tree
+        public int Pass { get; set; }
+
+        // The number of times a lower case letter ends in a node of prefix tree
+        public int End { get; set; }
+        
+        // The 26 paths for 26 lower case letters. Ex. Children[0] is the path of 'a'
+        // Children[i] == null then the path to i does not exist
+        // Children[i] != null then the path ot i exists
+        public TrieNode[] Children { get; }
+
+        public TrieNode()
+        {
+            this.Pass = 0;
+            this.End = 0;
+            this.Children = new TrieNode[26];
+        }
+
+    }
+}
